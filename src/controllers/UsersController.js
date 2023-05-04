8c5580ebf670dc02f047137fe22ab3429d6c050b
WagnerCSN <wagcostvez@gmail.com>
2023-05-03 22:39:27 -0300

    add usersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,26 @@
+const {hash} = require('bcryptjs');
+const knex = require('../database/knex');
+const AppError = require('../utils/AppError')
+
 class UsersController{
-    create(request, response){
-        const {name, email, senha} = request.body;
-        response.json({name, email, senha})
+  async  create(request, response){
+        const {name, email, password} = request.body;
+        
+       
+
+        const hashedpassword = await hash(password, 8);
+        
+        const checkUserExist = await knex("users").select('*').where('email', email).first();
+       
+        if(checkUserExist){
+            throw new AppError("Email já existe!")
+        }
+        await knex("users").insert({
+            name, 
+            email, 
+            password: hashedpassword,
+        })
+        response.json();
     }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require('express-async-errors')
+const AppError = require('./utils/AppError')
 const express = require('express');
 const routes = require('./routes')
 const app = express();
@@ -7,5 +9,21 @@ app.use(express.json());
 const Port = 3333;
 
 app.use(routes);
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError){
+        response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message
+        })
+    }
+
+    console.error(error);
+
+    response.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+    })
+})
 
 app.listen(Port, () => console.log(`Server is running on Port ${Port}`));
